4b2b0e64839115dee01183316aa5926f21cf7f1c
MikeBanister <118936528+MikeBanister@users.noreply.github.com>
2023-07-24 16:41:07 -0600

    html skeleton

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+import math
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+# Base Route
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# API Route
+@app.route("/api")
+def api():
+    return 
+
+# Run
+if __name__ == "__main__":
+    app.run()
