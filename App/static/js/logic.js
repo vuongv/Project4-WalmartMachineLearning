0ebfe07d343323ebe1f3cb0ffa84d15ba7861571
MikeBanister <118936528+MikeBanister@users.noreply.github.com>
2023-07-31 18:51:02 -0600

    added descriptions

--- App/static/js/logic.js
+++ App/static/js/logic.js
@@ -32,7 +32,6 @@ $("#nav3").on("click", function() {
 
 
 // STORE TRENDS DATA
-
 const dropdown = document.getElementById('store-dropdown');
 const option = document.createElement('option');
 
@@ -42,47 +41,54 @@ d3.json("/../../../Data/all_store_trend_seasonal.json").then(function (data) {
     // SECOND DATASET
     d3.json("/../../../Data/all_store_trend_seasonal.json").then(function (data_2) {
 
-
         // LOAD DROPDOWN WITH STORES
         for (let i = 1; i < 46; i++) {
             const option = document.createElement('option');
             option.value = i;
             option.textContent = `Store ${i}`;
             dropdown.appendChild(option);
-        }
+        };
 
         // DETECT STORE CHANGE
         dropdown.addEventListener('change', StoreChange);
-        function StoreChange(event) {
-            const storeNumber = event.target.value;
-            console.log(storeNumber);
+        
+        // CHANGE FUNCTION
+        function StoreChange() {
+            const storeNumber = dropdown.value
+            ChangeCharts(storeNumber);
+        };
 
-            // LINE CHART
+        function ChangeCharts(Number) {
+
+            // TREND CHART
             var trace1 = [{
-                x: data["Store_"+storeNumber+"_Trend"]["Date"],
-                y: data["Store_"+storeNumber+"_Trend"]["Trend"],
+                x: data["Store_"+Number+"_Trend"]["Date"],
+                y: data["Store_"+Number+"_Trend"]["Trend"],
                 line: {width:3},
+                hovertemplate: 'Sales: %{y:,.2f}<extra></extra>',
             }]
             var layout1 = {
-                title: "Store "+storeNumber,
+                title: "Store " + Number + " — Trending",
                 font: {color: "#ffffff"},
-                paper_bgcolor: 'rgba(0,0,0,0)',
+                paper_bgcolor: 'rgba(0,0,0,0.05)',
                 plot_bgcolor: 'rgba(0,0,0,0)',
             };
-            Plotly.newPlot("trend-chart", trace1, layout1)
-            
+            TrendChart = Plotly.newPlot("trend-chart", trace1, layout1)
+
+            // SEASONALITY CHART
             var trace2 = [{
-                x: data["Store_"+storeNumber+"_Trend"]["Date"],
-                y: data["Store_"+storeNumber+"_Seasonal"]["Seasonal"],
+                x: data["Store_"+Number+"_Trend"]["Date"],
+                y: data["Store_"+Number+"_Seasonal"]["Seasonal"],
                 line: {width:3},
+                hovertemplate: 'Sales: %{y:,.2f}<extra></extra>',
             }]
             var layout2 = {
-                title: "Store "+storeNumber,
+                title: "Store " + Number + " — Seasonality",
                 font: {color: "#ffffff"},
-                paper_bgcolor: 'rgba(0,0,0,0)',
+                paper_bgcolor: 'rgba(0,0,0,0.05)',
                 plot_bgcolor: 'rgba(0,0,0,0)',
             };
-            Plotly.newPlot("seasonality-chart", trace2, layout2)
+            SeasonalityChart = Plotly.newPlot("seasonality-chart", trace2, layout2)
 
 
 
@@ -90,11 +96,12 @@ d3.json("/../../../Data/all_store_trend_seasonal.json").then(function (data) {
 
 
         }; // EVENT CHANGE
+
+        // AUTOMATICALLY LOAD STORE 1
+        ChangeCharts(1);
+        
     }); // DATA 2
 }); // DATA 1
-
-   
-
 
 
 
